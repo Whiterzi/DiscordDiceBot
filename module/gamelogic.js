45162ebe392671ad56d6f41ab486893dede142f4
RETIYM\Whiter <catwhiter@gmail.com>
2022-07-06 22:07:01 +0800

    hotfix 6

--- module/gamelogic.js
+++ module/gamelogic.js
@@ -80,7 +80,7 @@ function BetResult(result) {
                 }
                 break;
             case '6':
-                if (v.bet === '6') {
+                if (v.bet === result + '') {
                     return betWinning(v.id, v.bet, v.amount, 6)
                 }
                 break;
